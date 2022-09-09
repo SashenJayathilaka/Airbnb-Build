8deffc34815177e9ad2a7a5fb8817e80e043eee7
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2022-09-09 16:57:06 +0530

    💠 Airbnb Build

--- components/MediumCard.tsx
+++ components/MediumCard.tsx
@@ -12,14 +12,10 @@ const MediumCard: React.FC<MediumCardProps> = ({ img, title }) => {
       className="cursor-pointer hover:scale-105 transform transition 
     duration-300 ease-out"
     >
-      <div className="relative h-80 w-80">
-        <img
-          src={img}
-          //layout="fill"
-          className="rounded-xl"
-        />
+      <div className="relative h-60 w-80">
+        <Image src={img} layout="fill" className="rounded-xl" />
       </div>
-      <h3 className="text-2xl mt-3">{title}</h3>
+      <h3 className="text-xl mt-3 text-gray-700 font-semibold">{title}</h3>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Banner from "../components/Banner";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
@@ -28,7 +29,7 @@ const Home = ({ exploreData, cardData }: HomeProps) => {
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
-          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <h2 className="text-3xl font-semibold pb-5">Explore Nearby</h2>
           {/* data from sever */}
           <div
             className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
@@ -36,7 +37,7 @@ const Home = ({ exploreData, cardData }: HomeProps) => {
           >
             {exploreData?.map((item: any) => (
               <SmallCard
-                key={item._id}
+                key={item.img}
                 img={item.img}
                 distance={item.distance}
                 location={item.location}
@@ -45,13 +46,13 @@ const Home = ({ exploreData, cardData }: HomeProps) => {
           </div>
         </section>
         <section>
-          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <h2 className="text-3xl font-semibold py-8">Live Anywhere</h2>
           <div
             className="flex space-x-3 overflow-scroll scrollbar-hide p-3
           -ml-3"
           >
             {cardData?.map((item: any) => (
-              <MediumCard key={item._id} img={item.img} title={item.title} />
+              <MediumCard key={item.img} img={item.img} title={item.title} />
             ))}
           </div>
         </section>
@@ -62,6 +63,7 @@ const Home = ({ exploreData, cardData }: HomeProps) => {
           buttonText="Get Inspired"
         />
       </main>
+      <Footer />
     </div>
   );
 };
